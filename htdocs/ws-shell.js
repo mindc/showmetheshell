813da0935f1b99f376f721fd3675e73b73865626
mindc <mindc@o2.pl>
2016-07-20 08:25:26 +0200

    trying to fix crossbrowser keycodes

--- htdocs/ws-shell.js
+++ htdocs/ws-shell.js
@@ -30,6 +30,7 @@ function Shell(o) {
 
     self.bind = function() {
         $(document).bind('keyup', function (e) {
+        
 			$("#shell-cursor").addClass("blink")        
             var code = e.keyCode || e.which;
             if (code == 27) {
@@ -37,19 +38,59 @@ function Shell(o) {
             }
         });
 
+        $(document).keydown(function (e) {
+			DEBUG && console.log( "KEYDOWN => keyCode: " +  e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which )        
+            var code = (e.keyCode ? e.keyCode : e.which);
+            switch ( code ) {
+				case 8:
+				case 9:
+				case 13:
+				case 33:
+				case 34:
+				case 35:
+				case 36:
+				case 37:
+				case 38:
+				case 39:
+				case 40:
+				case 45:
+				case 46:
+				
+				case 112:
+				case 113:
+				case 114:
+				case 115:
+				case 116:
+				case 117:
+				case 118:
+				case 119:
+				case 120:
+				case 121:
+				case 122:
+				case 123:
+					self.sendMessage({"type":"key","code":code});
+					return false;
+				default:
+            }
+        })
+
+
+
         $(document).bind('keypress', function(e) {
+			DEBUG && console.log( "KEYPRESS => keyCode: " +  e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which )        
 			$("#shell-cursor").removeClass("blink")
-			if ( e.keyCode ) {
-				self.sendMessage({"type":"key","code":e.keyCode});
-			} else if ( e.charCode ) {
+//			if ( e.keyCode ) {
+	//			self.sendMessage({"type":"key","code":e.keyCode});
+			//} else if ( e.charCode ) {
 				code = e.charCode;
+
 				if ( e.ctrlKey ) {
 					if ( code >= 97 ) {
 						code -= 96
 					}
 				}
 				self.sendMessage({"type":"char","code":code});
-			}
+		//	}
 
             return false;
         });
